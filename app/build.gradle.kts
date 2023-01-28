c0a07058606c883e61b5641e748e3750149b9747
Kenneth Mathari <mathari580@gmail.com>
2023-01-28 22:01:20 +0300

    Configure Apollo Kotlin dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("androidx.navigation.safeargs.kotlin")
+    id("com.apollographql.apollo3").version("3.7.4")
 }
 
 android {
@@ -43,8 +44,17 @@ dependencies {
     implementation("androidx.paging:paging-runtime-ktx:3.1.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha03")
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
+
+apollo {
+    service("service") {
+        packageName.set("com.example.rocketreserver")
+    }
+}
+
+
